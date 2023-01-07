301bc070fcf8ff1123fe4d91b5549700f7d9f0f8
Oleg Bagirov <88608903+bagirovoleg@users.noreply.github.com>
2023-01-07 19:03:48 +0100

    Version 1.0 -Designing models and creating model transfers -Working with requests and managers -Create views, templates, and URLs -Adding pagination -Using Django class based views

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/kiwi/PycharmProjects/Blog_Django1.8.6_Python3.5/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
